eb6abf3523e95e98c5e4169f606cd2b23fce6e6f
Keredu <luiscortesferre@gmail.com>
2019-05-22 17:49:52 +0200

    Task 1

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-Juego de la vida
-================
+# Repository of [Curso de Programación en C](https://1984.lsi.us.es/curso-programacion-c/)
+
+## Getting Started
+
+Get your own copy with:
+```
+git clone https://github.com/Keredu/luis_cortes.git
+```
+### Prerequisites
+
+A C compiler, in my case, gcc.
+
+## Running Conway's Game of Life
+
+Compiling and running the Conway's Game of Life:
+```
+gcc main.c -o conway
+./conway
+```
+
+## Authors
+
+* **Luis Cortés Ferre** - *Initial work* - [Keredu](https://github.com/Keredu)
+
+
+
--- main.c
+++ main.c
@@ -1,85 +1,103 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <assert.h>
 
-void gol_init(/* Recibo un mundo */);
-void gol_print(/* Recibo un mundo */);
-void gol_step(/* Recibo dos mundos */);
-int gol_count_neighbors(/* Recibo un mundo y unas coordenadas */);
-bool gol_get_cell(/* Recibo un mundo y unas coordenadas */);
-void gol_copy(/* Recibo dos mundos */);
+#define MAX_DIST 1
+#define ROWS 40
+#define COLUMNS 40
+#define TOROID 0
+
+void gol_init(bool *);
+void gol_print(bool *);
+void gol_step(bool *, bool *);
+char gol_count_neighbors(bool *, char, char);
+bool gol_get_cell(bool *, char, char);
+void gol_copy(bool *, bool *);
+
+bool init[3][3] = {
+    {0, 1, 0},
+    {0, 0, 1},
+    {1, 1, 1}
+};
 
 int main()
 {
 	int i = 0;
-	// TODO: Declara dos mundos
-
-	// TODO: inicializa el mundo
+	bool board[ROWS * COLUMNS];
+    bool board_copy[ROWS * COLUMNS];
+	gol_init(board);
 	do {
 		printf("\033cIteration %d\n", i++);
-		// TODO: Imprime el mundo
-		// TODO: Itera
+		gol_print(board);
+		gol_step(board, board_copy);
 	} while (getchar() != 'q');
 
 	return EXIT_SUCCESS;
 }
 
-void gol_init(/* Recibo un mundo */)
+void gol_init(bool *board)
 {
-	// TODO: Poner el mundo a false
-
-	/* TODO: Inicializar con el patrón del glider:
-	 *           . # .
-	 *           . . #
-	 *           # # #
-	 */
+    char i, j;
+    for(i = 0; i < ROWS; i++)
+        for(j = 0; j < COLUMNS; j++)
+            board[ROWS * i + j] =(i < 3 && j < 3)?init[i][j]:0;
 }
 
-void gol_print(/* Recibo un mundo */)
+void gol_print(bool *board)
 {
-	// TODO: Imprimir el mundo por consola. Sugerencia:
-	/*
-	 *     . # . . . . . . . .
-	 *     . . # . . . . . . .
-	 *     # # # . . . . . . .
-	 *     . . . . . . . . . .
-	 *     . . . . . . . . . .
-	 *     . . . . . . . . . .
-	 *     . . . . . . . . . .
-	 *     . . . . . . . . . .
-	 *     . . . . . . . . . .
-	 *     . . . . . . . . . .
-	 */
+   for(char i = 0; i < ROWS; i++){
+        for (char j = 0; j < COLUMNS; j++)
+            printf("%c ",(board[ROWS * i + j])?'#':'.');
+        printf("\n");
+	    }
 }
 
-void gol_step(/* Recibo dos mundos */)
+void gol_step(bool *board, bool *board_copy)
 {
-	/*
-	 * TODO:
-	 * - Recorrer el mundo célula por célula comprobando si nace, sobrevive
-	 *   o muere.
-	 *
-	 * - No se puede cambiar el estado del mundo a la vez que se recorre:
-	 *   Usar un mundo auxiliar para guardar el siguiente estado.
-	 *
-	 * - Copiar el mundo auxiliar sobre el mundo principal
-	 */
+    char cell, neighbors;
+    gol_copy(board, board_copy);
+    for(char x = 0; x < ROWS; x++)
+        for(char y = 0; y < COLUMNS; y++){
+		    cell = board_copy[ROWS * x + y];
+			neighbors = gol_count_neighbors(board_copy, x, y) - cell;               
+            if (cell)
+                board[ROWS * x + y] = (neighbors == 2 || neighbors == 3);
+            else
+                board[ROWS * x + y] = neighbors == 3;
+        }
 }
 
-int gol_count_neighbors(/* Recibo un mundo y unas coordenadas */)
+char gol_count_neighbors(bool *board, char x, char y)
 {
-	// Devuelve el número de vecinos
+	char a, b, in_board;
+	char neighbors = 0;
+	for(char k = -MAX_DIST; k < MAX_DIST + 1; k++)
+	    for(char z = -MAX_DIST; z < MAX_DIST + 1; z++){
+	        if (TOROID){
+                a = (((x + k) % ROWS) + ROWS) % ROWS;
+                b = (((y + z) % COLUMNS) + COLUMNS) % COLUMNS;
+                neighbors += board[ROWS * a + b];
+            } else {
+                a = x + k;
+                b = y + z;
+                neighbors += gol_get_cell(board, a, b);
+           }
+		}
+	return neighbors;
 }
 
-bool gol_get_cell(/* Recibo un mundo y unas coordenadas */)
+bool gol_get_cell(bool *board, char x, char y)
 {
-	/*
-	 * TODO: Devuelve el estado de la célula de posición indicada
-	 * (¡cuidado con los límites del array!)
-	 */
+// assert(x >= 0 && x < ROWS && y >= 0 && y < COLUMNS);
+	if ((x >= 0 && x < ROWS && y >= 0 && y < COLUMNS) || TOROID)
+        return board[ROWS * x + y];
+    return 0;
 }
 
-void gol_copy(/* Recibo dos mundos */)
+void gol_copy(bool *board, bool *board_copy)
 {
-	// TODO: copia el mundo segundo mundo sobre el primero
+	for(char i = 0; i < ROWS; i++)
+    	for(char j = 0; j < COLUMNS; j++)
+        	board_copy[ROWS * i + j] = board[ROWS * i + j];
 }
